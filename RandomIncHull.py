dc1be2855337add5d085b39d20b72065c114e460
Denver Persinger <denver.persinger@du.edu>
2025-03-18 11:36:55 -0600

    add docstring comments to RandHull and update test file to show hull and show convexity

--- RandomIncHull.py
+++ RandomIncHull.py
@@ -5,6 +5,12 @@ import random
 
 class RandomIncrementalHull3D:
     def __init__(self, points):
+        """
+        Initializes the Random Incremental Hull object and creates the hull
+
+        Args:
+            points (3 dimensional tuples): A list of points that the user wants a hull made from
+        """
         self.points = list(map(lambda x: Vertex(x), points))
         self.hull = DCEL()
         random.shuffle(points)
@@ -18,13 +24,31 @@ class RandomIncrementalHull3D:
         
         for point in self.points[4:]: # O(nlgn)
             self.add_point(point)
+            
+    def get_hull(self):
+        """
+        Function to get the DCEL hull from the Object itself
 
-        # self.hull.plot() # visualize the hull
+        Returns:
+            DCEL: The DCEL representing the hull
+        """
+        return self.hull
+
+    def plot(self):
+        """
+        Function to plot the hull from the Object itself
+        """
+        self.hull.plot()
         
-        # print(helpers.is_convex(self.hull)) #used to ensure correct result
-
     def get_conflicts(self, points, faces):
-        points = set(points)
+        """
+        A function to update the conlict graph with new faces and vertices
+
+        Args:
+            points (List(Vetex)): The list of points that need new conflict faces
+            faces (List): The list of new faces that replaced the old faces of the above points
+        """
+        points = set(points) ## O(n) for O(1) removal
         for face in faces:
             verts = self.hull.get_face_vertices(face)
             points_to_remove = []
@@ -43,6 +67,16 @@ class RandomIncrementalHull3D:
 
                     
     def add_point(self, point):
+        """
+        A function to add a point to the hull incrementally, finding the horizon, and forming new faces
+        with every point on the horizon with the new point
+
+        Args:
+            point (Vertex): The next point to incrementally add to the hull
+
+        Returns:
+            None: No return type, simply updates the DCEL representing the hull
+        """
         if point not in self.conflict_vertices or self.conflict_vertices[point] is None:
             self.hull.get_or_create_vertex(point)
             return ## point is not in conflict, so it is indside the hull
@@ -59,12 +93,23 @@ class RandomIncrementalHull3D:
         self.new_faces = []
 
     def get_horizon(self, face, point):
+        """
+        Finds the horizon of visibility for a given point in the hull and removes the faces within
+        the horizon to prepare for the addition of new faces
+
+        Args:
+            face (Face): The first face found in the visibility graph for the point
+            point (Vertex): The point to add to the hull, to find the horizon of its visibility
+
+        Returns:
+            List: A not neccesarly ordered list of edges that form the horizon of the visibility of the point
+        """
         horizon_edges = set()  
         visited_faces = set()  # explored faces
         queue = deque()  
         to_remove = []
 
-        # Start BFS from first visible face
+        # Start BFS from first visible face (from conflict graph)
         queue.append(face)
         visited_faces.add(face)
         while queue:
@@ -104,4 +149,4 @@ class RandomIncrementalHull3D:
             self.hull.remove_face(face)
         return list(horizon_edges)
 
-RandomIncrementalHull3D(helpers.generate_random_points(71))
+RandomIncrementalHull3D(helpers.generate_random_points(100))
--- test.py
+++ test.py
@@ -18,22 +18,37 @@ def time_algorithm(n_values, i):
         results.append(avg_time)
         if total_time > 2:
             iterations = iterations//2
-        print(f"n = {n:5d} | Average Time = {avg_time:.4f} sec over {iterations} iterations | Total Time = {total_time:.4f} sec")
+        print(f"n = {n:5d} | Average Time = {avg_time:.4f} sec over {iterations:2d} iterations | Total Time = {total_time:.4f} sec")
         total_time = 0
   
-    return results
+    # Plot the runtime, seems to be O(n)
+    plt.figure(figsize=(10, 6))
+    plt.plot(n_values, results, marker='o', linestyle='-', color='b')
+    plt.xlabel('Number of Points (n)')
+    plt.ylabel('Average Time (seconds)')
+    plt.title('Runtime of RandomIncrementalHull3D')
+    plt.grid(True)
+    plt.show()
 
-n_values = [500, 1000, 2000, 4000, 8000, 16000, 32000, 64000]
-results = time_algorithm(n_values, 75)
+def visualize_hull(n: int):
+    points = helpers.generate_random_points(n)
+    hull = RandomIncrementalHull3D(points)
+    hull.plot()
+    print(helpers.is_convex(hull.get_hull())) ##verify correctness of the algorithm
 
-# Plot the runtime, seems to be O(n)
-plt.figure(figsize=(10, 6))
-plt.plot(n_values, results, marker='o', linestyle='-', color='b')
-plt.xlabel('Number of Points (n)')
-plt.ylabel('Average Time (seconds)')
-plt.title('Runtime of RandomIncrementalHull3D')
-plt.grid(True)
-plt.show()
+
+def main():
+    n_values = [500, 1000, 2000, 4000, 8000, 16000, 32000, 64000]
+    n = 100
+    time_algorithm(n_values, 75)
+    visualize_hull(n)
+    
+    
+if __name__ == '__main__':
+    main()
+    
+
+    
 
 
 
